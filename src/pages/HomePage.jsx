e5ebf816799a9976384850fa09944c38562031b4
sofiane berkane <sofianeberkane202@gmail.com>
2024-12-11 16:12:06 +0100

    finish building style of contnet of homePage

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -3,12 +3,29 @@ import styles from './HomePage.module.css';
 /* eslint-disable react/react-in-jsx-scope */
 function HomePage() {
     return (
-        <div className={styles.homePage}>
+        <div className={`flex flex-col ${styles.homePage}`}>
             {/* navBar */}
             <NavBar/>
             {/* content */}
-            
+            <Content/>
         </div>
+    )
+}
+
+function Content(){
+    return(
+        <div className={`flex flex-col flex-center-y ${styles.content}`}>
+            <h1>
+                <span>You travel the world.</span>
+                <span>WorldWise keeps track of your adventures.</span>
+            </h1>
+            <p>
+                A world map that tracks your footsteps into every city you can
+                think of. Never forget your wonderful experiences, and show your
+                friends how you have wandered the world.
+            </p>
+            <button>start tracking now</button>
+         </div>
     )
 }
 
