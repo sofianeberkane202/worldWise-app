1d28465fbe1540a6845ea8b097c47e3f6d3ef82d
sofiane berkane <sofianeberkane202@gmail.com>
2024-12-11 19:48:20 +0100

    start building content of product page component

--- src/pages/ProductsPage.jsx
+++ src/pages/ProductsPage.jsx
@@ -5,6 +5,27 @@ function ProductsPage() {
     return (
         <div className={styles.products}>
             <NavBar/>
+            <div className={`${styles.content} flex flex-center-y`}>
+                <div className={styles.image}>
+                    <img 
+                    src='../../public/img-1.jpg' 
+                    alt='products'
+                    width={450}
+                    height={450}
+                    />
+                </div>
+
+                <div>
+                    <h2>About WorldWide.</h2>
+                    <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo est dicta illum vero culpa cum quaerat architecto sapiente eius non soluta, molestiae nihil laborum, placeat debitis, laboriosam at fuga perspiciatis?
+                    </p>
+                    <p>
+                    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Corporis doloribus libero sunt expedita ratione iusto, magni, id sapiente sequi officiis et.
+                    </p>
+                </div>
+
+            </div>
         </div>
     )
 }
