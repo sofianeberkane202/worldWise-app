e319a5443230c91c86a37046da10bd19edc6b319
sofiane berkane <sofianeberkane202@gmail.com>
2024-12-12 11:14:34 +0100

    finish styling the login form of a login page and also doing some update on the styling in other components

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,10 +1,32 @@
 import styles from './LoginPage.module.css'
 import NavBar from '../components/NavBar'
+import Button from '../components/Button'
 /* eslint-disable react/react-in-jsx-scope */
 function LoginPage() {
     return (
         <div className={styles.login}>
+            {/* navBar */}
             <NavBar/>
+
+            {/* login form */}
+            <form>
+                {/* email address */}
+                <div className='flex flex-col'>
+                    <label htmlFor='email'>Email address</label>
+                    <input type='email' id='email' value={'jack@example.com'}/>
+                </div>
+
+                {/* Password */}
+
+                <div className='flex flex-col'>
+                    <label htmlFor='password'>Password</label>
+                    <input type='password' id='password' value={'12345678'}/>
+                </div>
+
+                {/* login button */}
+                <Button type={'primary'}>Login</Button>
+            </form>
+
         </div>
     )
 }
