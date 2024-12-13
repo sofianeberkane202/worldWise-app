f73217f4c573413c40ab174546244ae9f758c24e
sofiane berkane <sofianeberkane202@gmail.com>
2024-12-13 17:38:45 +0100

    add cities context API to Map component by using cities data

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -3,63 +3,15 @@
 import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet'
 import PropTypes from 'prop-types'
 import styles from './Map.module.css'
+import { useContext } from 'react';
+import { ContextCities } from '../App';
 
-const cities= {
-    "cities": [
-      {
-        "id": "8631",
-        "cityName": "Wielichowo",
-        "country": "Poland",
-        "emoji": "🇵🇱",
-        "date": "2024-12-09T08:01:08.028Z",
-        "notes": "amizing",
-        "position": {
-          "lat": "52.106395654383256",
-          "lng": "16.327977153884454"
-        }
-      },
-      {
-        "id": "fcde",
-        "cityName": "Civril",
-        "country": "Turkiye",
-        "emoji": "🇹🇷",
-        "date": "2024-12-09T08:01:33.697Z",
-        "notes": "normal",
-        "position": {
-          "lat": "38.175217134937206",
-          "lng": "29.68966322211157"
-        }
-      },
-      {
-        "id": "f921",
-        "cityName": "Cheminon",
-        "country": "France",
-        "emoji": "🇫🇷",
-        "date": "2024-12-09T08:07:19.675Z",
-        "notes": "",
-        "position": {
-          "lat": "48.731974812301495",
-          "lng": "4.916356449203186"
-        }
-      },
-      {
-        "id": "8c7f",
-        "cityName": "Hrodna",
-        "country": "Belarus",
-        "emoji": "🇧🇾",
-        "date": "2024-12-09T08:15:46.990Z",
-        "notes": "normal",
-        "position": {
-          "lat": "53.79481713296426",
-          "lng": "23.811629264193233"
-        }
-      }
-    ]
-  }
 
 function Map() {
+    const {cities}= useContext(ContextCities);
+
     const position = [51.505, -0.09];
-    const positions = cities["cities"].map(city => {
+    const positions = cities.map(city => {
         const {position:{lat,lng}} = city;
         return {
             position: [+lat,+lng],
