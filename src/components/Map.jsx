c1bdfb30a0cf87d721ffe51306e00fe0421b255e
sofiane berkane <sofianeberkane202@gmail.com>
2024-12-14 20:31:37 +0100

    add a new feature is a map click

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,15 +1,19 @@
 /* eslint-disable react/react-in-jsx-scope */
 
-import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet'
+import { MapContainer, Marker, Popup, TileLayer, useMapEvent } from 'react-leaflet'
 import PropTypes from 'prop-types'
 import styles from './Map.module.css'
 import {useCities } from '../context/ContextCities';
+import { useState } from 'react';
 
 
 function Map() {
     const {cities}= useCities();
+    // eslint-disable-next-line no-unused-vars
+    const [positionClick, setPositionClick]= useState();
 
     const position = [51.505, -0.09];
+
     const positions = cities.map(city => {
         const {position:{lat,lng}} = city;
         return {
@@ -18,6 +22,13 @@ function Map() {
             emoji: city.emoji,
         }
     });
+
+    function ClickHandler(){
+        useMapEvent({
+            click: (e) => setPositionClick(e.latlng)
+        })
+    }
+
     return (
         <div className={`${styles.map} flex-1`}>
             <MapContainer 
@@ -29,6 +40,9 @@ function Map() {
                 attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
                 url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                 />
+
+                <ClickHandler/>
+
                 <Marker position={position}>
                 <Popup>
                     A pretty CSS3 popup. <br /> Easily customizable.
--- src/context/ContextCities.jsx
+++ src/context/ContextCities.jsx
@@ -1,20 +1,13 @@
 /* eslint-disable react/react-in-jsx-scope */
 import PropTypes from 'prop-types'
 
-<<<<<<< HEAD
 import { createContext, useContext, useReducer } from "react"
-=======
-import { createContext, useContext, useEffect, useReducer } from "react"
->>>>>>> loading
 import { URL } from '../config';
 import { fetchData } from '../helper';
 
 const initialReducer = {
   cities: [],
-<<<<<<< HEAD
   city: {},
-=======
->>>>>>> loading
   isLoading: false,
   error: null,
 }
@@ -22,28 +15,18 @@ const initialReducer = {
 function reducer(state, action) {
   switch(action.type){
     case 'loading': return {...state, isLoading: true};
-<<<<<<< HEAD
     case 'GET_CITIES_SECCESS': return {...state, isLoading: false, cities: action.payload};
     case 'GET_CITY_SECCESS': return {...state, isLoading: false, city: action.payload};
-=======
-    case 'success': return {...state, isLoading: false, cities: action.payload};
->>>>>>> loading
     case 'error': return {...state, isLoading: false, error: action.payload};
   }
 }
 
 const ContextCities = createContext();
 function ContextCitiesProvider({children}) {
-<<<<<<< HEAD
-=======
-
-    const [{cities, isLoading ,error},dispatch]= useReducer(reducer,initialReducer);
->>>>>>> loading
 
     const [{cities,city, isLoading ,error},dispatch]= 
       useReducer(reducer,initialReducer);
 
-<<<<<<< HEAD
     // getCities Data 
     async function fetchCitiesData(){
       dispatch({type: 'loading'});
@@ -68,37 +51,16 @@ function ContextCitiesProvider({children}) {
       }
   }
 
-=======
-    useEffect(function(){
-      async function fetchCitiesData(){
-        try {
-          dispatch({type: 'loading'});
-          const data = await fetchData(URL);
-          
-          dispatch({type: "success", payload: data});
-        } catch (error) {
-          console.log(error);
-        }
-      }
-  
-      fetchCitiesData();
-    },[]);
->>>>>>> loading
     
     return (
         <ContextCities.Provider
         value={{
             cities,
-<<<<<<< HEAD
             city,
             isLoading,
             error,
             fetchCitiesData,
             fetchCityData
-=======
-            isLoading,
-            error
->>>>>>> loading
         }}
         >
             {children}
